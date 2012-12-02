f980d58e281ef2fc0f773b606612432e6d592460
Katarina <katarina@katarina-VirtualBox.(none)>
2012-12-02 20:30:45 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doggable::Application.config.secret_token = 'd79eaaa399697f3b4e53f600f75a832a0685aa591d92d2e37ab46ca89ed4c8157ea16d0d16396a8284c3e7160d6f6352a5547f6e5039ba0eb86c32bdd35f2923'
